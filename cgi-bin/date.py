35bc9bdaf9165e9c09072700a2b0143273a8764d
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-08-05 10:08:21 -0500

    Actually remove the call to birthday(1).

--- cgi-bin/date.py
+++ cgi-bin/date.py
@@ -6,5 +6,4 @@ import cgi
 print "Content-type: text/html"
 print ""
 date_string = subprocess.check_output("date")
-birthday_string = subprocess.check_output("birthday")
-print cgi.escape(date_string + '\n' + birthday_string).replace("\n", "\n<br>")
+print cgi.escape(date_string)
