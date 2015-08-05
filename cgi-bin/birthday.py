401256c477e4ccc5295cc76a9afb97b959348019
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-08-05 10:03:04 -0500

    Add date to output.

--- cgi-bin/birthday.py
+++ cgi-bin/birthday.py
@@ -5,5 +5,6 @@ import cgi
 
 print "Content-type: text/html"
 print ""
+date_string = subprocess.check_output("date")
 birthday_string = subprocess.check_output("birthday")
-print cgi.escape(birthday_string).replace("\n","<br>\n")
+print cgi.escape(date_string + '\n' + birthday_string).replace("\n", "\n<br>")
