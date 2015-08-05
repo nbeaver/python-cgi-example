4f5f6f957ad215a4200cf451d8d6ffd500d7d80b
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-08-05 04:21:09 -0500

    Add <br> tags.

--- cgi-bin/birthday.py
+++ cgi-bin/birthday.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python2
 
 import subprocess
+import cgi
 
 print "Content-type: text/html"
 print ""
 birthday_string = subprocess.check_output("birthday")
-print birthday_string
+print cgi.escape(birthday_string).replace("\n","<br>\n")
