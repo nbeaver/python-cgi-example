b1024b5bbb7d12c43dd8c7534092c5ad7ba18fcb
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-04-04 11:16:00 -0500

    Better file handling and integer conversion.

--- cgi-bin/hit_counter.py
+++ cgi-bin/hit_counter.py
@@ -9,14 +9,18 @@ cgitb.enable()
 hit_count_path = os.path.join(os.path.dirname(__file__), "hit-count.txt")
 
 if os.path.isfile(hit_count_path):
-    hit_count = int(open(hit_count_path).read())
+    with open(hit_count_path) as fp:
+        hit_count_str = fp.read()
+        try:
+            hit_count = int(hit_count_str)
+        except ValueError:
+            hit_count = 0
     hit_count += 1
 else:
     hit_count = 1
 
-hit_counter_file = open(hit_count_path, 'w')
-hit_counter_file.write(str(hit_count))
-hit_counter_file.close()
+with open(hit_count_path, 'w') as fp:
+    fp.write(str(hit_count))
 
 header = "Content-type: text/html\n\n"
 
